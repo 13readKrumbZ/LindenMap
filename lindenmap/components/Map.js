4efb5341afd308bdd4b3b24278a5e1435000f074
De-von <specktraomega@gmail.com>
2025-11-05 19:48:45 -0300

    get Coordinates on click

--- lindenmap/components/Map.js
+++ lindenmap/components/Map.js
@@ -1,9 +1,41 @@
+//create function that handles placing markers on the map
+//the fuction should: get on click coordinates
+// place marker on coordinates
+
+//Plan
+//create react state as an empty array to hold markers
+//add coordinates from get coordinates to the array
+
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import MapMenue from "./MapMenue";
 
 export default function Map() {
+  const [markers, setMarkers] = useState([
+    { geocode: [6.002, -58.302], popup: "Marker 1" },
+    { geocode: [6.005, -58.305], popup: "Marker 2" },
+    { geocode: [6.008, -58.31], popup: "Marker 3" },
+  ]);
+
+  function createMarker(latlng) {
+    setMarkers((prev) => [
+      ...prev,
+      {
+        geocode: [latlng.lat, latlng.lng],
+        popup: `Marker ${prev.length + 1}`,
+      },
+    ]);
+  }
+
   return (
     <MapContainer
       center={[6, -58.3]}
@@ -16,6 +48,24 @@ export default function Map() {
         url="http://localhost:3001/tiles/{z}/{x}/{y}.png"
         attribution="&copy; Linden Map"
       />
+
+      <MapMenue />
+
+      <DetectClick onMapClick={createMarker} />
+
+      {markers.map((marker) => (
+        <Marker position={marker.geocode}></Marker>
+      ))}
     </MapContainer>
   );
+}
+
+function DetectClick({ onMapClick }) {
+  useMapEvents({
+    click: (e) => {
+      onMapClick?.(e.latlng);
+      console.log(e);
+    },
+  });
+  return null;
 }
---
+++ lindenmap/components/MapMenue.js
@@ -0,0 +1,10 @@
+import "./components.css";
+export default function MapMenue() {
+  return (
+    <div className="MapMenue">
+      <h2>Map Menue</h2>
+      <button>Add Marker</button>
+      <button>Remove Marker</button>
+    </div>
+  );
+}
